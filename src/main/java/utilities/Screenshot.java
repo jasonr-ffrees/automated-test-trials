3156e5d4bb18f579077f5f9ab17774dec50741bb
Jason Roy <jason.roy@ffrees.co.uk>
2014-12-04 09:23:53 +0000

    Image robot test for jenkins

--- src/main/java/utilities/Screenshot.java
+++ src/main/java/utilities/Screenshot.java
@@ -8,8 +8,15 @@ import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 import org.testng.Reporter;
 
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Screenshot extends TestListenerAdapter{
 	
@@ -18,21 +25,32 @@ public class Screenshot extends TestListenerAdapter{
 	{
 		if(!tr.isSuccess())
 		{
-	
+			String imagePath = "screenshots/" + tr.getTestClass().getName() + "/";
 			
-			WebDriver driver = TestBase.getDriverInstance();
-			File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-			String destDir = "target/surefire-reports/screenshots/" + tr.getTestClass().getName();
+			BufferedImage img = null;
+			try{
+				Robot r = new Robot();
+				Toolkit t = Toolkit.getDefaultToolkit();
+				Rectangle rec = new Rectangle(t.getScreenSize());
+				img = r.createScreenCapture(rec);				
+			} catch(AWTException e) {
+				e.printStackTrace();
+			}			
+			
+			String destDir = "target/surefire-reports/" + imagePath;
 			new File(destDir).mkdirs();
-			String destFile = "failure.png";
+			String destFile = "failure.jpg";
 		
 			try{
-				FileUtils.copyFile(srcFile, new File(destDir + "/" + destFile));
+				File output = new File(destDir + "/" + destFile);
+				ImageIO.write(img, "jpg", output);
 			} catch(IOException e) {
 				e.printStackTrace();
 			}
-		
-			Reporter.log("<a href=../screenshots/" + tr.getTestClass().getName() + "/" + destFile + ">" + tr.getMethod().getMethodName() + "</a>");
+			
+			String imageHref = "../" + imagePath + destFile;
+			//create thumbnail
+			Reporter.log("<a href=" + imageHref + "><img src=" + imageHref + " width=200 height=200></img><br/>" + tr.getTestClass().getName() + "</a>");
 		}
 	}
 }
